0f4cefb29d97a130ddb61bcee7de0e6d5aa24de5
everyone-candoit <cnoriega494@gmail.com>
2025-07-20 15:54:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var password = 12345;
+
+function passcheck () {
+  
+  
+  if (document.getElementById('pass1'). value != password) {
+    
+    alert ('wrong password, Try again.');
+    
+    return false;
+    
+  }
+    
+  if (document.getElementById('pass1'). value == password) {
+    
+    
+    alert ('Correct Password. Click OK to enter.');
+  }
+}
